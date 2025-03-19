83423655046eb33bf9ae604f6ad97233810b9128
Elliot Nguyen <nguyenduykiet72@gmail.com>
2025-03-19 19:29:26 +0700

    feat(CP-34): Implement profile user

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
     pathname?.startsWith('/courses/create') || pathname?.startsWith('/register');
   return (
     <html lang="en">
-      <body className="font-roboto-condensed">
+      <body className="font-robotoCondensed">
         {isDifferentLayout ? children : <Layout>{children}</Layout>}
       </body>
     </html>
